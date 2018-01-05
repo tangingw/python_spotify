5758373dae7a7bcca627895d3fa3e0dbeda8bc42
Mobieus Jay <tangingw.pas@gmail.com>
2018-01-05 12:19:03 +0800

    First appearance on github

---
+++ README.md
@@ -0,0 +1,49 @@
+## SPOTIFY API Grabber written in Flask (Python 3.6)
+
+### Note
+This is an experimental version of SPOTIFY API grabber. The author will constantly update the features. Use it as your own cause.
+
+### Description
+This is a simple [Spotify](https://www.spotify.com) API grabber that retrieves data from its own API [endpoint](https://beta.developer.spotify.com). As this software is still in its very early stage, the author advices not to use it for production, however the author will constantly update this repository as the project is meant to be in production.
+
+### Synopsis
+The purpose of building this API grabber is the first building block that the author wants to build for his own voice recognized Spotify player. Eventhough there are few items such as Alexa, Cortana, Google Home existed in the market, but not everyone can afford such devices, as well as not everyone can afford to have Spotify Premium account. The project uses Spotify Web API to control different devices through [Spotify Connect](https://www.spotify.com/my-en/connect/).
+
+The author is currently swallowing javascript and jQuery so that can come out with a frontend UI that can control the usage of the API through Spotify Connect.
+
+### Before you **git clone**
+Before you download this repository, make sure that:
+* Register yourself in the [Spotify Developer](https://beta.developer.spotify.com)
+* Create your App in Spotify
+* Whitelist your callback in this format *http://example.com/callback/* (a / at the back is a must)
+* Grab both **client_id** and **client_secret**
+
+### Once you **git clone**
+Edit config.json
+```json
+    {
+        "callback_url": "http://example.com/callback/",
+        "scope": "user-read-email user-read-birthdate user-read-playback-state",
+        "state_msg": "<YOUR FAVOURITE MESSAGE>"
+    }
+```
+
+Edit credentials.json
+```json
+    {
+        "client_id": "<YOUR CLIENT_ID>",
+        "client_secret": "<YOUR CLIENT_SECRET>"
+    }
+```
+
+For Spotify OAuth2 scopes, please refer to the [scope](https://developer.spotify.com/web-api/using-scopes/) page. 
+Do not worry about token.json, it is a file that used by system to store token related data.
+
+### After you **git clone**
+On your terminal, run 
+```bash
+    SHELL> cd ~/python_spotify
+    SHELL> python app.py
+```
+### Pull Request, Anyone?
+contact me at [tangingw.pas@gmail.com](mailto:tangingw.pas@gmail.com)
---
+++ app.py
@@ -0,0 +1,188 @@
+import hashlib
+import base64
+import json
+import os
+import time
+import requests
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import redirect
+from flask import url_for
+
+
+app = Flask(__name__)
+
+
+CRED_CONTENT = json.loads(open("credentials.json", "r").read())
+CLIENT_ID = CRED_CONTENT["client_id"]
+CLIENT_SECRET = CRED_CONTENT["client_secret"]
+
+
+BASE_URL = "https://api.spotify.com"
+AUTHORIZE_URL = "https://accounts.spotify.com/authorize/"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+
+
+CONFIG = json.loads(open("config.json", "r").read())
+CALLBACK_URL = CONFIG["callback_url"]
+SCOPE = CONFIG["scope"]
+STATE_MSG = CONFIG["state_msg"]
+
+
+def write_file(post_data):
+
+    secret_string = base64.b64encode(bytes((CLIENT_ID + ":" + CLIENT_SECRET).encode("utf-8")))
+
+    headers = {
+        "Authorization": "Basic %s" % secret_string.decode("utf-8")
+    }
+
+    response = requests.post(TOKEN_URL, headers=headers, data=post_data)
+
+    current_time = int(time.time())
+    expiry_time = current_time + 3600
+
+    new_response = json.loads(response.text)
+    new_response.update(
+        {
+            "start_time": current_time,
+            "expiry_time": expiry_time
+        }
+    )
+
+    with open("token.json", "w") as file:
+
+        file.write(json.dumps(new_response))
+
+    return redirect(url_for("get_spotify"))
+
+
+def generate_header():
+
+    if not os.path.exists("token.json"):
+
+        return redirect(url_for("authorize"))
+
+    with open("token.json", "r") as file:
+
+        file_data = file.read()
+
+    if not file_data:
+
+        return redirect(url_for("authorize"))
+
+    data = json.loads(file_data)
+
+    if int(time.time()) > data["expiry_time"]:
+
+        return redirect(url_for("refresh_token"))
+
+    headers = {
+        "Authorization": "Bearer " + data["access_token"]
+    }
+
+    return headers, data
+
+
+@app.route("/")
+def hello_world():
+
+    return "Hello World!"
+
+
+@app.route("/authorize")
+def authorize():
+
+    params = {
+        "client_id": CLIENT_ID,
+        "response_type": "code",
+        "redirect_uri": CALLBACK_URL,
+        "state": hashlib.sha256(STATE_MSG.encode('utf-8')).hexdigest(),
+        "scope": SCOPE
+    }
+
+    response = requests.get(AUTHORIZE_URL, params=params)
+
+    return redirect(response.url)
+
+
+@app.route("/callback/")
+def callback():
+
+    callback_params = request.args
+
+    if "error" in callback_params:
+
+        return jsonify(
+            error=callback_params.get("error"),
+            state=callback_params.get("state")
+        )
+
+    post_data = {
+        "grant_type": "authorization_code",
+        "code": callback_params.get("code"),
+        "redirect_uri": CALLBACK_URL,
+    }
+
+    return write_file(post_data)
+
+
+@app.route("/spotify")
+def get_spotify():
+
+    headers, data = generate_header()
+
+    if int(time.time()) > data["expiry_time"]:
+
+        return redirect(url_for("refresh_token"))
+
+    response = requests.get(BASE_URL + "/v1/me/player", headers=headers)
+
+    if not response.text:
+
+        response = requests.get(BASE_URL + "/v1/me/player/devices", headers=headers)
+
+    my_device = json.loads(response.text)
+
+    #return json.dumps(myown_data, indent=4, ensure_ascii=False)
+    return jsonify(my_device)
+
+
+@app.route("/spotify/me")
+def get_spotify_me():
+
+    headers, data = generate_header()
+
+    if int(time.time()) > data["expiry_time"]:
+
+        return redirect(url_for("refresh_token"))
+
+    response = requests.get(BASE_URL + "/v1/me", headers=headers)
+    myown_data = json.loads(response.text)
+
+    return jsonify(myown_data)
+
+
+@app.route("/refresh_token")
+def refresh_token():
+
+    with open("token.json", "r") as file:
+
+        data = json.loads(file.read())
+
+    if "refresh_token" not in data:
+
+        return redirect(url_for("authorization"))
+
+    post_data = {
+        "grant_type": "refresh_token",
+        "refresh_token": data["refresh_token"]
+    }
+
+    return write_file(post_data)
+
+
+if __name__ == "__main__":
+
+    app.run(host='0.0.0.0', port=8080)
